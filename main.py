a239bf8e39f3334f84bd1b9ccd2b0b7ee2893c7b
warmcup <74030965+warmcup@users.noreply.github.com>
2022-10-29 01:45:00 -0500

    Implementado modulo hashes.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import modules.pe as pe
+import modules.hashes as hashes
 import argparse
 
 if __name__ == '__main__':
@@ -7,12 +8,31 @@ if __name__ == '__main__':
     desc = '''
     
     '''
+    info_hash = ''
+    desc_hash = '''Ejemplos de uso:
+            + Obtener una lista de hashes de los archivos en todas las subcarpetas de C:/Documentos/trabajos/
+                hash -m dump -o hashesBase1.txt -r C:/Documentos/trabajos
+            + Comparar los hashes de los archivos en todas las subcarpetas de C:/Windows/users/files/datos/, con los almacenados en hashesBase1.txt
+                hash --mode comp --hashfile hashesGuardados.txt --ruta C:/Windows/users/files/datos
+                '''
+
     parser = argparse.ArgumentParser(epilog=desc)
     subparsers = parser.add_subparsers(dest='command', help='Funciones')
     parser_pe = subparsers.add_parser('analyze', help='Analiza un ejecutable portable, y genera su reporte con los resultados')
     parser_pe.add_argument('--exepath', required=True, help='Ruta del ejecutable portable a analizar')
     parser_pe.add_argument('--outprefix', required=False, help='Prefijo para el nombre de archivo del reporte generado')
-
+    parser_hash = subparsers.add_parser('hash', help='Crea y compara hashes sha512 de todos los archivos en una carpeta', epilog=desc_hash, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser_hash.add_argument('-m', '--mode', dest='mode',
+                        required=True,
+                        help="Modo de ejecucion: 'dump (obtener), comp (comparar)")
+    parser_hash.add_argument('-r', '--ruta', dest='path',
+                        required=True, 
+                        help="Especifica la carpeta para la obtencion de hashes (ruta absoluta)")
+    parser_hash.add_argument('-o', '--outfile', dest='outfile',
+                        help="Nombre del archivo a guardar con hashes (necesario para el modo dump)")
+    parser_hash.add_argument('-ha', '--hashfile', dest='hashfile',
+                        help="Nombre del con hashes contra el cual comparar (necesario para el modo comp)")
+        
     args = parser.parse_args()
 
     if args.command:
@@ -21,5 +41,21 @@ if __name__ == '__main__':
                 pe.analyzePEExe(args.exepath, args.outprefix)
             else:
                 pe.analyzePEExe(args.exepath)
+        if args.command == 'hash':
+            if args.mode == 'dump':
+                if args.outfile:
+                    hashes.dumpHashes(args.path, args.outfile)
+                else:
+                    print('Se requiere un argumento para el parametro --outfile en el modo dump.')
+            if args.mode == 'comp':
+                if args.hashfile:
+                    result = hashes.compareHashes(args.path, args.hashfile)
+                    if result != None:
+                        if result:
+                            print('Coinciden los hashes')
+                        else:
+                            print('No coinciden')
+                else:
+                    print('Se requiere un argumento para el parametro --hashfile en el modo dump.')
 
     
---
+++ modules/hashes.py
@@ -0,0 +1,62 @@
+import hashlib
+import os
+import pickle
+
+#Validando la ruta de la carpeta
+def ValidatePath(thePath):
+    ''' Validate the Folder thePath
+        it must exist and we must have rights
+        to read from the folder.
+        raise the appropriate error if either
+        is not true
+    '''
+    # Valida que la ruta exista
+    if not os.path.exists(thePath):
+        raise FileNotFoundError('La ruta no está disponible')
+
+    # Valida que la ruta es leible
+    if os.access(thePath, os.R_OK):
+        return thePath
+    else:
+        raise FileNotFoundError('La ruta no está disponible')
+
+# Cambiando la ruta de la carpeta con archivos a hashear
+# se crea un diccionario con los hashes y nombres de archivos
+def Hashes(ruta):
+    # set para que no importen los nombres de los archivos
+    setHashes = set()
+    os.chdir(ruta)
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            try:
+                with open(os.path.join(root, name), 'rb') as file:
+                    datos = file.read()
+                    hash = hashlib.sha512(datos)
+                    hasheado = hash.hexdigest()
+                    setHashes.add(hasheado)
+            except Exception as e:
+                print("Ocurrió un error inesperado: " + str(e))
+    return setHashes
+#=====================================================
+
+# Funciones que ejecutan dependiendo del modo del script
+
+# Dumpea un archivo en formato pickle el set con hashes
+def dumpHashes(path, outfile):
+    hashes = Hashes(path)
+    with open(outfile, 'wb') as out:
+        pickle.dump(hashes, out)
+
+
+def compareHashes(path, path_comp):
+    # Obtiene un set con hashes, carga el ubicado en path_comp (pickled), y los compara
+    hashes = Hashes(path)
+    try:
+        with open(path_comp, 'rb') as archivo1:
+            setHashes = pickle.load(archivo1)
+            if type(setHashes) == set:
+                return setHashes == hashes
+    except Exception as er:
+        print("Excepcion: " + str(er))
+    return None
+
