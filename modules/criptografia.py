0f8c7fa925e9a8589e528e89ead6ac7b510c52b4
BsIsraU08 <111823376+BsIsraU08@users.noreply.github.com>
2022-11-04 16:52:20 -0600

    Update criptografia.py

--- modules/criptografia.py
+++ modules/criptografia.py
@@ -1,5 +1,6 @@
 #Autor: Jesús Israel Bolaños Uvalle
 
+import argparse
 import re
 import os
 import errno
@@ -14,220 +15,184 @@ expcorreos=r'[a-z0-9!#$%&*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&*+/=?^_`{|}~-]+)*@(?:[a
 
 expIp= r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
 
-expusuarios=  r'user=([a-zA-Z0-9!#$%&*+/=?^_`.@{|}~-]+)'
+expusuarios=  r'(user=[a-zA-Z0-9!#$%&*+/=?^_`.@{|}~-]+)'
 
 
                   ############################################### BLOQUE DE FUNCIONES #######################################################
 
-#Funcion para buscar el archivo en toda la PC
-def buscaruta(ruta):
-    target = ruta
-    initial_dir = 'C:\\'
-
-    path = ''
-    for root, _, files in os.walk(initial_dir):
-        if target in files:
-           global archivo
-           archivo = os.path.join(root, target)
-           break
-
-#Funcion para retroceder un directorio antes de la carpeta, salirme de ella        
-def separador():
-    separador = os.path.sep
-    dir_actual = os.path.dirname(os.path.abspath('DATOS'))
-    global directorio
-    directorio = separador.join(dir_actual.split(separador)[:-1])
-
-
 #Función para la extracción de los datos
-def recoleccion():
+def recoleccion(archivo, llave):
     while True:
-        nombre= input("Nombre del archivo en donde se realizará la extracción: ")
-    
-        #Llamamos la funcion "buscaruta" para buscar el archivo en toda la PC
-        print("Buscando archivo...\n")
-        buscaruta(nombre)
       
         #Lee el contenido del archivo
         with open(archivo,'r') as file:
             contenido= file.read()
             
-        #Buscamos los correos en el contenido
-        buscar=re.findall(expregular, contenido)
-        #print(buscar)
+        #Buscamos los correos, ip y usuarios en el archivo
+        buscar1=re.findall(expregular1, contenido)
+        buscar2=re.findall(expregular2, contenido)
+        buscar3=re.findall(expregular3, contenido)
         
-        #Verificamos que haya encontrado coincidencias con la regex
-        w= len(buscar)
-        if len(buscar) > 0:
-            
-        #Creo una carpeta para ahi meter la key y el archivo con la información encriptada
-        #Compruebo si exite la carpeta ya creada
-            
-            try:
-                os.mkdir('DATOS')
-                a = os.path.abspath('DATOS')
-                os.chdir(a)
-            except FileExistsError:
-                a = os.path.abspath('DATOS')
-                os.chdir(a)
-            
+        #Creamos la carpeta DATOs
+        try:
+            os.mkdir('DATOS')
+            a = os.path.abspath('DATOS')
+            os.chdir(a)
+        except FileExistsError:
+            a = os.path.abspath('DATOS')
+            os.chdir(a)
+                
+    #La creación de la llave
+        def genwrite():
+            key = Fernet.generate_key()
+            with open(llave, "wb") as key_file:
+                key_file.write(key)
 
-            for x in buscar:
-                with open(nomdatos, 'a') as file:
-                    file.write(x)
-                    file.write('\n')
-            encriptar(nomdatos)
-            separador()
-            os.chdir(directorio)
-            break
-        print("\n##### No se encontraron coincidencias del dato solicitado #####\n")
-        print("##### Ingrese otro archivo #####\n")
-        continue
+        genwrite()
+    
+    # Leemos el archivo que tiene la llave
+        def call_key():
+            return open(llave, "rb").read()
+
+        key= call_key()
+        llave = Fernet(key)
+
+    # Buscamos los correos, ip y usuarios, dependiendo cual encuentre lo va agarrar en una archivo en especifico
+        for x in buscar1:
+            nom1= 'Correos.txt'
+            with open('Correos.txt', 'a') as file:
+                file.write(x)
+                file.write('\n')
+        encriptar(nom1, llave)
+
+        for x in buscar2:
+            nom2='Ips.txt'
+            with open('Ips.txt', 'a') as file:
+                file.write(x)
+                file.write('\n')
+        encriptar(nom2, llave)
+
+        for x in buscar3:
+            nom3='Usuarios.txt'
+            with open('Usuarios.txt', 'a') as file:
+                file.write(x)
+                file.write('\n')
+        encriptar(nom3, llave)
+
+        break
 
 
 
         
                         ####################################### BLOQUE DE ENCRIPTACION #######################################
     
-def encriptar(nom_archivo):
-    #La creación de la llave
-    llave1= input("Ingrese el nombre con el que quiera guardar su llave: ")
-    def genwrite():
-            key = Fernet.generate_key()
-            with open(llave1 + '.key', "wb") as key_file:
-                    key_file.write(key)
-
-    genwrite()
+def encriptar(nom_archivo, llave):
     
-    # Leemos el archivo que tiene la llave
-    def call_key():
-            return open(llave1 + '.key', "rb").read()
-    key= call_key()
-    a= Fernet(key)
     with open(nom_archivo,"rb") as file:
         archivo_info = file.read()
-    encrypted_data = a.encrypt(archivo_info)
+    encrypted_data = llave.encrypt(archivo_info)
     with open(nom_archivo, 'wb') as file:
         file.write(encrypted_data)
 
 
                             ####################################### BLOQUE DE DESENCRIPTACION ###############################################
                
-        
-def desencriptar(nom_archivo):
-    #Nos movemos a la carpeta en donde se encuentran los datos encriptados
-    a= os.path.abspath('DATOS')
-    os.chdir(a)
-    #print(a)
+def desencriptar(nom_carpeta, llave):
+
+    os.chdir(nom_carpeta)
+
+    #Llamamos a la llave para utilizarla
     def call_key():
-        llave=input("Ingrese el nombre del archivo que contiene su llave: ")
         return open(llave, "rb").read()
     
+    key= call_key()
+    llave = Fernet(key)
+    
     try:
-        key= call_key()
-        
-        a= Fernet(key)
-        try:
+        lista=[]
+
+        #Buscamos en el directorio todos los archivos que tengan la terminación .txt
+        for archivo in os.listdir():
+            if archivo.endswith(".txt"):
+                lista.append(archivo)
+
+        #Cada archivo que se encontro se le va a desencriptar
+        for archivo in lista:
             #Desencriptamos los datos
-            with open(nom_archivo,"rb") as file:
+            with open(archivo,"rb") as file:
                 encrypted_data = file.read()
-            decrypted_data = a.decrypt(encrypted_data)
-            with open(nom_archivo, 'wb') as file:
+            decrypted_data = llave.decrypt(encrypted_data)
+            with open(archivo, 'wb') as file:
                 file.write(decrypted_data)
                 
-            #Activamos el excel para meter la información encriptada
-            with open(nom_archivo,'r') as file:
+        #Activamos el excel para meter la información desencriptada
+        reporte=Workbook()
+        hoja= reporte.active
+
+        hoja['B1'] = "CORREOS"
+        hoja['G1'] = "IPs"
+        hoja['J1'] = 'Usuarios'
+
+        #En cada archivo vamos a buscar si tiene correos, ip o usuarios
+        for archivo in lista:
+            with open(archivo,'r') as file:
                 contenido= file.read()
-            buscar=re.findall(expregular, contenido)
+            buscar1=re.findall(expregular1, contenido)
+            buscar2=re.findall(expregular2, contenido)
+            buscar3=re.findall(expregular3, contenido)
 
-            hoja['B1'] = "CORREOS"
-            hoja['G1'] = "IPs"
-            hoja['J1'] = 'Usuarios'
+            co= len(buscar1)
+            ip= len(buscar2)
+            us= len(buscar3)
 
-            if expregular == expcorreos:
-                long= len(buscar)
-                for x in range(long):
-                    hoja[f'A{x +2}'] = buscar[x]
-                
-            elif expregular == expIp:
-                long= len(buscar)
-                for x in range(long):
-                    hoja[f'F{x +2}'] = buscar[x]
-                    
-            elif expregular == expusuarios:
-                long= len(buscar)
-                for x in range(long):
-                    hoja[f'I{x +2}'] = buscar[x]
+        #Se agrega al excel lo que se encontró en el archivo   
+            for correo in range(co):
+                hoja[f'A{correo +2}'] = buscar1[correo]
 
-            separador()
-            os.chdir(directorio)
-        except:
-            print('\nEl archivo encriptado no se encuentra en la carpeta!')
-            separador()
-            os.chdir(directorio)
+            for ip in range(ip):
+                hoja[f'F{ip +2}'] = buscar2[ip]
+
+            for usuario in range(us):
+                hoja[f'I{usuario +2}'] = buscar3[usuario]
+
+        reporte.save("Reporte de Encriptación.xlsx")
+        
     except FileNotFoundError:
-        print('\nLa llave no se encuentra en la carpeta!\n')
-        print('Verificar el nombre correcto del archivo que contiene la llave!\n')
-        separador()
-        os.chdir(directorio)
-        exit
-        
+        print('\nHay un archivo que no se puede desencriptar')
+
                         ####################################### MENU ###############################################
-        
-print("Bienvenido a la tarea de encriptación de datos\n")
-y='1'
-reporte=Workbook()
-hoja= reporte.active
-while y == '1':
 
-    print("[1] ENCRIPTAR CORREOS")
-    print("[2] ENCRIPTAR IP")
-    print("[3] ENCRIPTAR Usuarios")
-    print("[4] DESENCRIPTAR CORREOS Y HACER UN REPORTE")
-    print("[5] DESENCRIPTAR IP Y HACER UN REPORTE")
-    print("[6] DESENCRIPTAR Usuarios Y HACER UN REPORTE")
-    #a= os.getcwd()
-    #print(a)
-    opciones= input("¿Qué desea hacer? ")
 
-    if opciones == '1':
-        expregular= expcorreos
-        nomdatos='Correos'
-        recoleccion()
-        print("\nEncriptación correcta!! :D\n")
-        
-    elif opciones == '2':
-        expregular= expIp
-        nomdatos='IPs'
-        recoleccion()
-        print("\nEncriptación correcta!! :D\n")
-        
-    elif opciones == '3':
-        expregular= expusuarios
-        nomdatos='Usuarios'
-        recoleccion()
-        print("\nEncriptación correcta!! :D\n")
-        
-    elif opciones == '4':
-        expregular= expcorreos
-        nom_archivo= input("Nombre del archivo encriptado: ")
-        desencriptar(nom_archivo)
-        print("\nDesencriptación correcta!! :D\n")
+parser_crip = argparse.ArgumentParser()
+parser_crip.add_argument('-m', '--modo', dest='modo',
+                        required=True,
+                        help="Modo de encriptar: encrp .  Modo desencriptar: desen .")
+                        
+parser_crip.add_argument('-a', '--archivo', dest='archivo',
+                        required=True, 
+                        help="En el modo de Encriptación, especifica el archivo en donde se realizará la extracción(ruta absoluta). En el modo de Desencriptacion, espcifica la carpeta en donde se encuentren los archivos encriptados(ruta absoluta).")
 
-    elif opciones == '5':
-        nom_archivo= input("Nombre del archivo encriptado: ")
-        expregular= expIp
-        desencriptar(nom_archivo)
-        print("\nDesencriptación correcta!! :D\n")
-        
-    elif opciones == '6':
-        expregular= expusuarios
-        nom_archivo= input("Nombre del archivo encriptado: ")
-        desencriptar(nom_archivo)
-        print("\nDesencriptación correcta!! :D\n")
-        
+parser_crip.add_argument('-llv', '--llave', dest='llave',
+                        required=True, 
+                        help="Especifica el nombre de la llave")
+    
+args = parser_crip.parse_args()
+modo = args.modo
+archivo = args.archivo
+llave = args.llave
 
-    y= input("¿Desea hacer otra cosa? [1]SI, [2]NO: ")
-    #a= os.getcwd()
-    #print(a)
-reporte.save("Reporte de Encriptación.xlsx")
+if modo == 'encrp':
+    expregular1= expcorreos
+    expregular2= expIp
+    expregular3= expusuarios
+
+    recoleccion(archivo, llave)
+    print("\nEncriptación correcta!! :D\n")
+        
+elif modo == 'desen':
+    expregular1= expcorreos
+    expregular2= expIp
+    expregular3= expusuarios
+    
+    desencriptar(archivo, llave)
+    print("\nDesencriptación correcta!! :D\n")
