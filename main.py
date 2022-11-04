ff2192a37af49e829d3095069fa0a405bf904c62
BsIsraU08 <111823376+BsIsraU08@users.noreply.github.com>
2022-11-04 17:18:08 -0600

    Correciones

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     
     ejemplos_cripto = '''Ejemplos de uso:
             + Encriptar los datos encontrados en el archivo Ejem1.txt y guardar la llave de encripción con el nombre encript.key
-                   cripto -m encrp -a C:\Users\HP\Desktop\PIA\Ejem1.txt -llv encript.key
+                 cripto -m encrp -a C:\Users\HP\Desktop\PIA\Ejem1.txt -llv encript.key
             + Desencriptar los archivos que se encuentran en la carpeta DATOS con la llave encript.key
-                   cripto -m desen -a C:\Users\HP\Desktop\PIA\DATOS -llv encript.key     
-           
+                 cripto -m desen -a C:\Users\HP\Desktop\PIA\DATOS -llv encript.key     
+                 '''
 
     parser = argparse.ArgumentParser(epilog=desc)
     subparsers = parser.add_subparsers(dest='command', help='Funciones')
